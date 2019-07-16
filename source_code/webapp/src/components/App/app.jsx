1626da102fefb425708e3ced5c03a97fda44634b
Hoang Phu <hoangvanphu98@gmail.com>
2019-07-17 00:18:41 +0700

    inital web app v2

---
+++ source_code/webapp/src/components/App/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+import Dashboard from '../Dashboard/dashboard';
+
+function App() {
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
